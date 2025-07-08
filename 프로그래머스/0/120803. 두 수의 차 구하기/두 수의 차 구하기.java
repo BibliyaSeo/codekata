32827b3db8affb1c7d2f23e13a7936ebccc241c4
BibliyaSeo <111430820+BibliyaSeo@users.noreply.github.com>
2025-07-08 09:07:39 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.03 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.java
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.java
@@ -1,6 +1,12 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1-num2;
+        boolean booleanNum1 = num1>=-50000 && num1 <=50000;
+        boolean booleanNum2 = num2>=-50000 && num2 <=50000;
+
+        int answer = 0;
+        if(booleanNum1&&booleanNum2) {
+            answer = num1-num2;
+        }
         return answer;
     }
 }
