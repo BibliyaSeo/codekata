ff8487d4daa822797dc2658b42c6fd89f31c75d6
BibliyaSeo <111430820+BibliyaSeo@users.noreply.github.com>
2025-07-10 09:16:23 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -2,10 +2,11 @@ class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
         
-        if(num1 <= 0 || num1 > 100 || num2 <=0 || num2 > 100) {
-            System.out.println("1~100까지의 숫자를 입력해 주세요. 처리 불가로 답을 0으로 표현합니다.");
-                return answer;
+        if(num1 > 0 && num1 <= 100 && num2 > 0 && num2 <= 100) {
+       
+                return num1/num2;
         }
-        return num1/num2;
+        System.out.println("1~100까지의 숫자를 입력해 주세요. 처리 불가로 답을 0으로 표현합니다.");
+        return answer;
     }
 }
