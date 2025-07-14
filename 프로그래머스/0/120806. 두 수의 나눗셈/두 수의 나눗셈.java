7541e15b3d4d5228af23e0ab2f7be16516d4b0e4
BibliyaSeo <111430820+BibliyaSeo@users.noreply.github.com>
2025-07-14 09:11:59 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
-        double newNum1 = num1;
-        double nuwNum2 = num2;
-        answer = (int)(newNum1/nuwNum2 * 1000);
+        answer = (int)(num1/(double)num2 * 1000);
         return answer;
     }
 }
