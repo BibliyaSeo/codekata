e5b480d9fbeedcf87a79a9a3ce9543ff09043c4b
BibliyaSeo <111430820+BibliyaSeo@users.noreply.github.com>
2025-08-12 14:13:13 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 53.73 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,12 @@
 class Solution {
     public String solution(int n) {
-        String one = "수";
-        String two = "박";
         String answer = "";
         
         for(int i = 0; i<n; i++) {
             if(i%2==0) {
-                answer += one;
+                answer += "수";
             } else {
-                answer += two;
+                answer += "박";
             }
         }
         
